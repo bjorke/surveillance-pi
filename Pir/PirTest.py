e4810ad2ee9d4c125cd6d7fb3af513d3afccc1fc
bjorke <bjrkie@gmail.com>
2014-06-02 20:29:11 +0200

    changed logging

--- Pir/PirTest.py
+++ Pir/PirTest.py
@@ -15,7 +15,7 @@ while True:
     time.sleep(0.1)
     prevState = currState
     currState = GPIO.input(sensorPin)
-    print(GPIO.input(sensorPin))
     if currState != prevState:
+        print(GPIO.input(sensorPin))
         newState = "HIGH" if currState else "LOW"
         print "GPIO pin %s is %s" % (sensorPin, newState)
