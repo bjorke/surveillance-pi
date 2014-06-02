03ee4fb6490562f8efe073fe487686cc75a7eb5f
bjorke <bjrkie@gmail.com>
2014-06-02 21:28:29 +0200

    updates

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 import BoardNumbers as BoNu
+import sys
+sys.path.append("/home/pi/surveillance-pi/Pir")
+import Pir
+
 sonarEcho = 0
 sonarTrigger = 0
 pirEcho = 0
 
+def controlPir
+  intVal = Pit.getPirStatus(pirEcho)
+  return intVal
 
 if __name__ == "__main__":
   global sonarEcho
@@ -12,4 +19,5 @@ if __name__ == "__main__":
   sonarEcho = BoNu.sonarEchoPort()
   sonarTrigger = BoNu.sonarTriggerPort()
   pirEcho = BoNu.pirPort()
-  print(sonarEcho,sonarTrigger,pirEcho)
+
+  print(controlPir())
