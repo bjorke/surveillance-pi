d825d6215256912b76a3299e3dde661b515c22f5
bjorke <bjrkie@gmail.com>
2014-06-02 21:12:33 +0200

    updates

--- Main.py
+++ Main.py
@@ -4,8 +4,18 @@ sonarEcho = 0
 sonarTrigger = 0
 pir = 0
 
+def sonarEchoPort():
+  intVal = BoNu.sonarEchoPort
+  return intVal
+def sonarTriggerPort():
+  intVal = BoNu.sonarTriggerPort
+  return intVal
+def pirPort():
+  intVal = BoNu.pirPort
+  return intVal
+
 if __name__ == "__main__":
-  global sonarEcho = BoNu.sonarEchoPort
-  global sonarTrigger = BoNu.sonarTriggerPort
-  global pir = BoNu.pirPort
+  global sonarEcho = sonarEchoPort
+  global sonarTrigger = sonarTriggerPort
+  global pir = pirPort
   print(sonarEcho,sonarTrigger,pir)
