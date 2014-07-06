bfd0505c1f2b5e5e3bb4a512c5a5282aece92f16
Bjorke <ludwig@lubj01-osx.local>
2014-07-06 21:11:50 +0200

    Added threading

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import BoardNumbers as BoNu
 import sys
+from threading import Thread, Event
+
 sys.path.append("/home/pi/surveillance-pi/Pir")
 import Pir
 sys.path.append("/home/pi/surveillance-pi/Sonar")
@@ -10,16 +12,31 @@ sonarEcho = 0
 sonarTrigger = 0
 pirEcho = 0
 
+threadsArray = []
+class startChildThread (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        if self.name == "forward":
+
+
 def getPirValue():
   return Pir.getPirStatus(pirEcho)
 
 def getSonarDistance():
   return Sonar.getDistance(sonarTrigger,sonarEcho)
-
-if __name__ == "__main__":
+def mainThread():
   sonarEcho = BoNu.sonarEchoPort()
   sonarTrigger = BoNu.sonarTriggerPort()
   pirEcho = BoNu.pirPort()
 
   print("Pir; " , getPirValue())
   print("Sonar; " , getSonarDistance())
+if __name__ == "__main__":
+   mainThreadRun = Thread(target=mainThread , args=())
+   #if mainThreadV.isAlive():
+      #mainThreadV.join()
+   #else:
+   mainThreadRun.start()
