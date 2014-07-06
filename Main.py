e03b1be2812ace2fd3b4dcb6410b2d69e272d4fc
Bjorke <ludwig@lubj01-osx.local>
2014-07-06 21:20:28 +0200

    updated threading

--- Main.py
+++ Main.py
@@ -12,6 +12,13 @@ sonarEcho = 0
 sonarTrigger = 0
 pirEcho = 0
 
+def getPirValue():
+  print("pir ehco; " , pirEcho)
+  return Pir.getPirStatus(pirEcho)
+def getSonarDistance():
+  print("sonar echo; " , sonarEcho)
+  return Sonar.getDistance(sonarTrigger,sonarEcho)
+
 threadsArray = []
 class startChildThread (threading.Thread):
     def __init__(self, threadID, name):
@@ -19,21 +26,29 @@ class startChildThread (threading.Thread):
         self.threadID = threadID
         self.name = name
     def run(self):
-        if self.name == "forward":
+        if self.name == "sonar":
+          print("Sonar; " , getSonarDistance())
 
+        if self.name == "pir":
+          print("Pir; " , getPirValue())
 
-def getPirValue():
-  return Pir.getPirStatus(pirEcho)
-
-def getSonarDistance():
-  return Sonar.getDistance(sonarTrigger,sonarEcho)
 def mainThread():
+  #global threadsArray
   sonarEcho = BoNu.sonarEchoPort()
   sonarTrigger = BoNu.sonarTriggerPort()
   pirEcho = BoNu.pirPort()
+  threadOne = startChildThread(1, "sonar")
+  threadTwo = startChildThread(2, "pir")
 
-  print("Pir; " , getPirValue())
-  print("Sonar; " , getSonarDistance())
+  if not threadOne.isAlive():
+    threadOne.start()
+    threadsArray.append(threadOne)
+  if not threadTwo.isAlive():
+    threadTwo.start()
+    threadsArray.append(threadTwo)
+
+
+
 if __name__ == "__main__":
    mainThreadRun = Thread(target=mainThread , args=())
    #if mainThreadV.isAlive():
