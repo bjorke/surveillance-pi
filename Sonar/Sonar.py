ca60fec53d5d444037610fb3d48d7d35764354de
Ludwig Björk <gusbjorklu@student.gu.se>
2014-12-27 18:16:10 +0100

    updates

--- Sonar/Sonar.py
+++ Sonar/Sonar.py
@@ -4,28 +4,33 @@ import time
 import RPi.GPIO as GPIO
 import Settings
 
-#using BCM GPIO reference
-GPIO.setmode(GPIO.BCM)
+
 
 def getDistance(sonarTrigger, sonarEcho):
-  GPIO.setup(sonarTrigger, GPIO.OUT)
-  GPIO.setup(sonarEcho, GPIO.IN)
+    #using BCM GPIO reference
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(sonarTrigger, GPIO.OUT)
+    GPIO.setup(sonarEcho, GPIO.IN)
 
-  GPIO.output(sonarTrigger, True)
-  time.sleep(0.00001)
-  GPIO.output(sonarTrigger, False)
+    GPIO.output(sonarTrigger, True)
+    time.sleep(0.00001)
+    GPIO.output(sonarTrigger, False)
 
-  sent = time.time()
+    sent = time.time()
 
-  while GPIO.input(sonarEcho)==0:
-      sent = time.time()
+    time.sleep(0.5)
 
-  while GPIO.input(sonarEcho)==1:
-      returned = time.time()
+    while GPIO.input(sonarEcho)==0:
+        sent = time.time()
 
-  #Calculate time
-  elapsed = returned-sent
+    while GPIO.input(sonarEcho)==1:
+        returned = time.time()
 
-  distance = elapsed * 34300 / 2
+    #Calculate time
+    elapsed = returned-sent
 
-  return distance
+    distance = elapsed * 34300 / 2
+
+    #reset GPIO
+    GPIO.cleanup()
+    return distance
